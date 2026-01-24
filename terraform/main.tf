18787eff9385336358a9bb6214865fae727a4c03
ASriVamsi11 <andavarapuvamsi3@gmail.com>
2026-01-24 16:09:40 -0700

    more typos fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_subnet" "public_a" {
     vpc_id = aws_vpc.this.id 
     cidr_block = "10.0.1.0/24"
-    availabilty_zone = data.aws_availability_zones.available.names[0]
+    availability_zone = data.aws_availability_zones.available.names[0]
     map_public_ip_on_launch = true
     tags = {Name = "${var.project_name}-public-a"}
 }
@@ -39,8 +39,8 @@ resource "aws_route" "public_internet" {
     gateway_id = aws_internet_gateway.igw.id
 }
 
-resource "aws_route_table_associatoin" "public_a" {
-    subnet_id = aws.subnet.public_a.id
+resource "aws_route_table_association" "public_a" {
+    subnet_id = aws_subnet.public_a.id
     route_table_id = aws_route_table.public.id
 }
 
